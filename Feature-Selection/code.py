70992e8fdc3d9ae2e79276a77338332ad5f27d0c
nagnath001 <nagnath001@users.noreply.github.com>
2019-12-08 18:42:20 +0530

    Code file created.

---
+++ Feature-Selection/code.py
@@ -0,0 +1,137 @@
+# --------------
+import pandas as pd
+from sklearn import preprocessing
+#path : File path
+# Code starts here
+# read the dataset
+dataset=pd.read_csv(path)
+# look at the first five columns
+print(dataset.head())
+# Check if there's any column which is not useful and remove it like the column id
+dataset.drop(columns='Id',inplace=True)
+
+# check the statistical description
+print(dataset.describe())
+
+
+# --------------
+# We will visualize all the attributes using Violin Plot - a combination of box and density plots
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+#names of all the attributes 
+cols = dataset.columns
+print(len(cols))
+#number of attributes (exclude target)
+size = len(cols)-1
+print(size)
+#x-axis has target attribute to distinguish between classes
+x = cols[size]
+
+#y-axis shows values of an attribute
+y = cols[0:size]
+print(x)
+#Plot violin for all attributes
+for i in range(0,size):
+     sns.violinplot(data=dataset,x=x,y=y[i])  
+     plt.show()
+
+
+# --------------
+import numpy
+
+upper_threshold = 0.5
+lower_threshold = -0.5
+
+
+
+# Code Starts Here
+
+# create a subset of dataframe with only the first 10 features
+subset_train = dataset.iloc[:, :10]
+
+# Calculate the Pearson correlation
+data_corr = subset_train.corr()
+
+# Plot a heatmap
+f, ax = plt.subplots(figsize = (10,8))
+sns.heatmap(data_corr,vmax=0.8,square=True);
+
+# List the correlation pairs
+correlation = data_corr.unstack().sort_values(kind='quicksort')
+
+# Select the highest correlation pairs using slicing
+corr_var_list = correlation[((correlation>upper_threshold) | (correlation<lower_threshold)) & (correlation!=1)]
+
+# Code ends here
+
+
+
+# --------------
+#Import libraries 
+from sklearn import cross_validation
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+# Identify the unnecessary columns and remove it 
+dataset.drop(columns=['Soil_Type7', 'Soil_Type15'], inplace=True)
+# Scales are not the same for all variables. Hence, rescaling and standardization may be necessary for some algorithm to be applied on it.
+X = dataset.drop('Cover_Type',1)
+Y= dataset['Cover_Type']
+X_train,X_test,Y_train,Y_test= train_test_split(X,Y,test_size = 0.2 , random_state = 0)
+#Standardized
+scaler=StandardScaler()
+#Apply transform only for continuous data
+X_train_temp = scaler.fit_transform(X_train.iloc[:,0:size])
+X_test_temp = scaler.fit_transform(X_test.iloc[:,0:size])
+#Concatenate scaled continuous data and categorical
+X_train1=numpy.concatenate((X_train_temp,X_train.iloc[:,size:]),axis=1)
+X_test1=numpy.concatenate((X_test_temp,X_test.iloc[:,size:]),axis=1)
+scaled_features_train_df=pd.DataFrame(X_train1,index=X_train.index,columns=X_train.columns)
+scaled_features_test_df=pd.DataFrame(X_test1,index=X_test.index,columns=X_test.columns)
+
+
+
+
+
+
+# --------------
+from sklearn.feature_selection import SelectPercentile
+from sklearn.feature_selection import f_classif
+
+
+# Write your solution here:
+
+# Code starts here
+skb = SelectPercentile(score_func=f_classif,percentile=20)
+predictors = skb.fit_transform(X_train1, Y_train)
+scores = list(skb.scores_)
+
+Features = scaled_features_train_df.columns
+
+dataframe = pd.DataFrame({'Features':Features,'Scores':scores})
+
+dataframe=dataframe.sort_values(by='Scores',ascending=False)
+
+top_k_predictors = list(dataframe['Features'][:predictors.shape[1]])
+
+print(top_k_predictors)
+
+# Code Ends here
+
+
+# --------------
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score
+clf = LogisticRegression(random_state=0, multi_class='ovr')
+clf1=LogisticRegression(random_state=0, multi_class='ovr')
+model_fit_all_features=clf1.fit(X_train,Y_train)
+predictions_all_features=clf1.predict(X_test)
+score_all_features=accuracy_score(predictions_all_features,Y_test)
+model_fit_top_features=clf.fit(scaled_features_train_df[top_k_predictors], Y_train)
+predictions_top_features=clf.predict(scaled_features_test_df[top_k_predictors])
+score_top_features=accuracy_score(predictions_top_features,Y_test)
+print(score_all_features)
+print(score_top_features)
+
+
